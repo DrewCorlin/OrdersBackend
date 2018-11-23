18b36d08a87a40f2ced0d1f9020a6abaec596c49
Drew Corlin <andrewcorlin@gmail.com>
2018-11-23 02:00:46 -0800

    updated devinterceptor

--- grails-app/controllers/DevInterceptor.groovy
+++ grails-app/controllers/DevInterceptor.groovy
@@ -3,16 +3,17 @@ package com.orders
 import org.springframework.beans.factory.annotation.Value
 
 class DevInterceptor {
-    @Value('$app.environment.adminClearanceLevel')
-    private static final String ADMIN_LEVEL_CLEARANCE
+
+    @Value('${app.environment.adminClearanceLevel}')
+    String ADMIN_LEVEL_CLEARANCE
 
     DevInterceptor() {
         matchAll()
     }
 
     boolean before() {
+        println ADMIN_LEVEL_CLEARANCE
         if (request.JSON.adminLevel != ADMIN_LEVEL_CLEARANCE) {
-            println ADMIN_LEVEL_CLEARANCE
             log.error "Invalid access attempted"
             return false
         }
