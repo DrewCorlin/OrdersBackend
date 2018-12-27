e8a8c4c1b8b6c38ba1261a5091013f8b311d1c7c
Drew Corlin <andrewcorlin@gmail.com>
2018-12-27 01:59:24 -0800

    password hashing

--- grails-app/domain/User.groovy
+++ grails-app/domain/User.groovy
@@ -1,42 +1,28 @@
 package com.orders
 
-// import grails.plugin.springsecurity.SpringSecurityService
-// import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder
 
 class User {
-    // @Autowired
-    // SpringSecurityService springSecurityService
 
     static constraints = {
         name unique: true
         roles minSize: 1
+        passwordSecured nullable: true
     }
 
     String name
-    List<String> roles = [] // Id's of role objects
-    transient String password // Updated this to only store password hash
+    List<String> roles = []
+    transient String password
     String passwordSecured
 
     String id
     Date lastUpdated
     Date dateCreated
 
-    // void setPassword(String newPass) {
-    //     println newPass
-    //     println "----"
-    //     println springSecurityService
-    //     println "++++++"
-    //     passwordSecured = springSecurityService.encodePassword(name, newPass)
-    //     println passwordSecured
-    // }
-
-    // boolean isCorrectPassword(String candidate) {
-    //     println candidate
-    //     println "========"
-    //     println springSecurityService.encodePassword(name, candidate)
-    //     println "========"
-    //     return passwordSecured == springSecurityService.encodePassword(name, candidate)
-    // }
+    void setPassword(String newPass) {
+        LdapShaPasswordEncoder lspe = new LdapShaPasswordEncoder()
+        passwordSecured = lspe.encodePassword(newPass, name as byte[])
+    }
 
     @Override
     String toString() {
